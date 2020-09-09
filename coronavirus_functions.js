8c8f6d775045813945ebe2c4eefcc6f7e03f235b
Johann.Rajakarunanayake <jrajakarunanayake@gmail.com>
2020-09-09 14:43:57 +0100

    first commit

---
+++ coronavirus_functions.js
@@ -0,0 +1,96 @@
+export { fetchData, formatCursor, addMapFeatures }
+
+//fetches json data from json files
+async function fetchData(data) {
+    let fetchedData = await fetch(data);
+    let json = await fetchedData.json();
+    let features = json.features;
+    return features
+}
+
+
+//function that formats the cursor when hovering over a marker
+function formatCursor(cursor, map) {
+    event.preventDefault();
+    map.getCanvas().style.cursor = cursor;
+    event.stopPropagation();
+}
+
+
+function addBoroughsOutline(map, id, boroughPolygons, lineWidth) {
+    map.addLayer({
+        'id': id,
+        'type': 'line',
+        'source': {
+            'type': 'geojson',
+            'data': {
+                "type": "FeatureCollection",
+                "features": boroughPolygons
+            }
+        },
+        'paint': {
+            'line-color': '#000',
+            'line-width': lineWidth
+        }
+    });
+}
+
+function addChoroplethLayer(map, id, boroughPolygons) {
+    map.addLayer({
+        "id": id,
+        "type": "fill",
+        "source": {
+            "type": "geojson",
+            "data": {
+                "type": "FeatureCollection",
+                "features": boroughPolygons
+            }
+        },
+        "layout": {},
+        "paint": {
+            "fill-color": '#FFF',
+            "fill-opacity": 0.6
+        }
+    });
+}
+
+
+
+function addMapFeatures(map) {
+    map.dragRotate.disable(); // Disable map rotation using right click + drag.
+    map.touchZoomRotate.disableRotation(); // Disable map rotation using touch rotation gesture.
+
+    // Add navigation control (excluding compass button) to the map.
+    map.addControl(new mapboxgl.NavigationControl({
+        showCompass: false
+    }));
+
+    map.on('click', 'boroughs', function(e){
+        let currentBorough = e.features;
+        
+
+        if (!map.getLayer('current-borough')) {
+            addBoroughsOutline(map, 'current-borough', currentBorough, 7);
+        }
+        else {
+            let currentBoroughData = {
+                "type": "FeatureCollection",
+                "features": currentBorough
+            }
+            map.getSource('current-borough').setData(currentBoroughData)
+        }
+        console.log(currentBorough[0].properties["NAME"])
+    })
+
+    map.on('load', async function () {
+        //Fetches the polygons of all the UK counties. 
+        let boroughPolygons = await fetchData('london_boroughs.json');
+        console.log(boroughPolygons)
+    
+        addChoroplethLayer(map, 'boroughs', boroughPolygons)
+        addBoroughsOutline(map, 'boroughs-outline', boroughPolygons, 3);
+        
+
+    });
+
+}
---
+++ coronavirus_map.js
@@ -0,0 +1,25 @@
+import { addMapFeatures } from "./coronavirus_functions.js"
+
+
+const API_KEY = "H1xjzAwzi4GGgXdlszV4nMri4CXd1Gmw"
+
+let vectorUrl = 'https://api.os.uk/maps/vector/v1/vts';
+
+// Initialize the map object.
+let map = new mapboxgl.Map({
+    container: 'map',
+    minZoom: 8,
+    maxZoom: 17,
+    style: vectorUrl + '/resources/styles?key=' + API_KEY,
+    center: [-0.1318827, 51.5158643],
+    zoom: 8,
+    transformRequest: url => {
+        if (! /[?&]key=/.test(url)) url += '?key=' + API_KEY
+        return {
+            url: url + '&srs=3857'
+        }
+    }
+});
+
+
+addMapFeatures(map)
